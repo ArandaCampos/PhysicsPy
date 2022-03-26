f75dcbd4013333c81829020e2eb0440d202f713b
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-03-26 14:28:54 -0300

    Versão 1.0

---
+++ src/main.py
@@ -0,0 +1,116 @@
+import numpy as np
+import pygame
+import math
+pygame.init()
+
+WIDTH, HEIGHT = 1200, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("MUV e MRU com projétil")
+
+#base_font = pygame.font.Font(None, 32)
+#user_text = ''
+
+WHITE = (210, 210, 210)
+BLACK = (0, 0, 0, .8)
+
+FONT = pygame.font.SysFont("comicsans", 16)
+
+class Objeto:
+	g = -9.807 		       	# Aceleração da gravidade (m/s^2)
+	ESCALA = 10				# 10px == 1 metro
+
+	def __init__(self, x, y, velocidade, angulo, raio):
+		self.x = x													# Posição Vertical (m)
+		self.y = y													# Posição Horizontal (m)
+		self.vy = velocidade * math.sin(math.radians(angulo))		# Velocidade Inicial eixo y
+		self.vx = velocidade * math.cos(math.radians(angulo))		# Velocidade Inicial eixo x
+		self.angulo = angulo
+		self.raio = raio 			# Raio (m)
+		self.orbita = []
+
+	def draw(self, win):
+		updated_points = []
+		for point in self.orbita:
+			x, y = point
+			x = x
+			y = y
+			updated_points.append((x, y))
+
+		if len(self.orbita) > 2:
+			pygame.draw.lines(win, BLACK, False, updated_points, 2)
+
+		pygame.draw.circle(win, BLACK, (x, y), self.raio)
+
+	def velocidade(self, t):
+		vx, vy, g = self.vx, self.vy, self.g
+		Vx = vx * t
+		Vy = vy * t + g * math.pow(t, 2) / 2
+		return Vx, Vy
+
+	def deslocamento(self, t):
+		vx, vy = self.velocidade(t)
+		x, y = self.x, self.y
+		px = x + vx
+		py = y + vy
+		return px, py
+
+	def update_position(self, t):
+		x, y = self.deslocamento(t)
+		if y >= 0:
+			print(f'{x} | {y}')
+			self.orbita.append((x * self.ESCALA , HEIGHT - y * self.ESCALA - self.raio))
+
+def main():
+	run = True
+	clock = pygame.time.Clock()
+	t = 0
+	objeto = Objeto(0, 10, 30, 45, 20)
+
+	while run:
+		clock.tick(20)
+		WIN.fill(WHITE)
+		t += 1/20
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+		objeto.update_position(t)
+		objeto.draw(WIN)
+
+		pygame.display.update()
+
+	pygame.quit()
+
+'''
+def menu():
+	clock = pygame.time.Clock()
+	clock.tick(20)
+	run = True
+
+	while run:
+		WIN.fill(WHITE)
+
+		input_box = pygame.Rect(100, 100, 0, 0)
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_BACKSPACE:
+					user_text = user_text[:-1]
+				else:
+					user_text += event.unicode
+
+			pygame.display.update()
+		t = user_text
+		text_surface = base_font.render('Ola', True, (255, 255, 255))
+		print(f'{t}')
+		WIN.blit(text_surface, (5, 5))
+
+	pygame.quit()
+
+#menu()
+'''
+main()
